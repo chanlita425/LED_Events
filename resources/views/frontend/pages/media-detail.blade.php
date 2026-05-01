2289e6ac4b67ff7bf3b26b8a000c826c37367c9d
git lysing098 <lyzing1021@gmail.com>
2026-05-01 17:25:46 +0700

    update behind the scenes

--- resources/views/frontend/pages/media-detail.blade.php
+++ resources/views/frontend/pages/media-detail.blade.php
@@ -18,11 +18,37 @@
 
         @foreach (explode("\n", $media->description_en) as $line)
             @if (trim($line))
-                <p class="text-gray-300 text-lg leading-relaxed mb-4">
+                <p class="prose text-gray-300 text-lg leading-relaxed mb-4">
                     {{ $line }}
                 </p>
             @endif
         @endforeach
+        @if (!empty($media->images))
+    <div class="max-w-6xl mx-auto px-5 pb-20">
+
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6 auto-rows-[200px]">
+
+            @foreach ($media->images as $index => $img)
+
+                <div
+                    class="
+                        overflow-hidden rounded-xl
+                        {{ $index == 0 ? 'md:col-span-3 md:row-span-2' : '' }}
+                        {{ $index == 1 ? 'md:col-span-2' : '' }}
+                    "
+                >
+                    <img
+                        src="{{ Storage::url($img) }}"
+                        class="w-full h-full object-cover hover:scale-105 transition duration-300"
+                    >
+                </div>
+
+            @endforeach
+
+        </div>
+
+    </div>
+@endif
 
     </div>
 @endsection
