e4273b310793998c5d5b6b943b7eae15484afbbf
git lysing098 <lyzing1021@gmail.com>
2026-05-02 11:54:55 +0700

    update article page

--- resources/views/frontend/pages/blog-detail.blade.php
+++ resources/views/frontend/pages/blog-detail.blade.php
@@ -15,7 +15,7 @@
 
     <div class="max-w-4xl mx-auto py-20 text-white px-5">
 
-        @foreach (explode("\n", $article->description_en) as $line)
+        @foreach (explode("\n", $article->description_km) as $line)
             @if (trim($line))
                 <p class="prose text-gray-300 text-lg leading-relaxed mb-4">
                     {!! $line !!}
